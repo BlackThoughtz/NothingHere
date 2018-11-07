d5fa8a5e4a66e7c9df92e76a73bd17b2ea9e3e15
David Butler <butlrdav@gmail.com>
2018-11-07 18:01:59 -0500

    first commmit

---
+++ BarBeerDrinker/config.py
@@ -0,0 +1,1 @@
+database_uri = "mysql+pymysql://edb:OurPassword!@edbproject.chxmewtjeajj.us-east-2.rds.amazonaws.com:3306/BarsProject"
---
+++ BarBeerDrinker/database.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, sql
+
+from BarBeerDrinker import config
+
+engine = create_engine(config.database_uri)
+
+def get_bars():
+    with engine.connect() as con:
+        rs = con.execute("SELECT name, license, city, phone, addr FROM tbars;")
+        return [dict(row) for row in rs]
+
+def find_bar(name):
+    with engine.connect() as con:
+        query = sql.text(
+            "SELECT name, license, city, phone, addr FROM BARS WHERE name = :name;"
+        )
+        rs = con.execute(query, name = name)
+        result = rs.first()
+        if result is None:
+            return None
+        return dict(result)
+
+def filter_beers(max_price):
+    with engine.connect() as con:
+        query = sql.text(
+            "SELECT * FROM Sells WHERE price < :max_price;"
+        )
+
+        rs = con.execute(query, max_price=max_price)
+        results = [dict(row) for row in rs]
+        for r in results:
+            r['price'] = float(r['price'])
+
+        return results
